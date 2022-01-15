c9b8df1cf798484c69d37641d113fa1263038f04
fedor2018 <39495056+fedor2018@users.noreply.github.com>
2022-01-15 16:59:44 +0200

    Update ntc.py

--- rp2/lib/ntc.py
+++ rp2/lib/ntc.py
@@ -3,7 +3,7 @@ from math import log
 
 class NTC():
     """ """
-    def __init__(self, adc=None, Vref=3.3, R=10000, Ro=10000.0, To=25.0, beta=3950.0, V=5 ):
+    def __init__(self, adc=None, Vref=3.3, R=10000, Ro=10000.0, To=25.0, beta=3950.0, V=5, Vt=None, adc_t=None ):
         self._adc=adc
         self._vref=Vref
         self._r=R
@@ -11,6 +11,13 @@ class NTC():
         self._r0=Ro
         self._t0=To
         self._beta=beta
+        self._Vt=Vt   #example - tl431, 2.5V 
+        self._adc_t=adc_t #adc for correct Vref
+        
+    def vref_cor(self):
+        """ vref correct """
+        if(self._Vt is not None and self._adc_t is not None):
+            self._vref=self._Vt*65535/self._adc_t.read_u16()
         
     def r_UP(self):
         """
@@ -36,6 +43,7 @@ class NTC():
 #        return self._r / (65535/self._adc.read_u16())# - 1)
     
     def in_volt(self):
+        self.vref_cor()
         return self._adc.read_u16()*(self._vref/65535)
          
     def to_temp(self, r):
